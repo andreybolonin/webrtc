7bd099571b906023dd86913a0a4d32b3abf67292
dmitry <dgulkovskiy@gmail.com>
2015-05-24 00:37:10 +0300

    no vars in global scope; call canceling added;

--- public/js/main.js
+++ public/js/main.js
@@ -1,65 +1,94 @@
-var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
-var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+var state = { // global var
+  inCall: false,
+  pc: null,  // PeerConnection
+  stream: null,
+  createAnswer: null,
+  sendMessage: null
+};
 
-var pc; // PeerConnection
+(function(){
 
+  var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 
-// Step 1. getUserMedia
-navigator.getUserMedia(
-  { audio: true, video: true }, 
-  gotStream, 
-  function(error) { console.log(error) }
-);
+  var callButton = document.getElementById("callButton");
+  var localVideo = document.getElementById("localVideo");
+  var remoteVideo = document.getElementById("remoteVideo");
+  var getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 
-function gotStream(stream) {
-  document.getElementById("callButton").style.display = 'inline-block';
-  document.getElementById("localVideo").src = URL.createObjectURL(stream);
+  // Step 1. getUserMedia
+  getUserMedia.call(navigator,
+    { audio: true, video: true }, 
+    gotStream, 
+    function(error) { console.log(error) }
+  );  
 
-  pc = new PeerConnection(null);
-  pc.addStream(stream);
-  pc.onicecandidate = gotIceCandidate;
-  pc.onaddstream = gotRemoteStream;
-}
+  function gotStream(stream) {
+    callButton.style.display = 'inline-block';
+    callButton.style.color = 'green';
+    localVideo.src = URL.createObjectURL(stream);
 
-
-// Step 2. createOffer
-function createOffer() {
-  pc.createOffer(
-    gotLocalDescription, 
-    function(error) { console.log(error) }, 
-    { 'mandatory': { 'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true } }
-  );
-}
-
-
-// Step 3. createAnswer
-function createAnswer() {
-  pc.createAnswer(
-    gotLocalDescription,
-    function(error) { console.log(error) }, 
-    { 'mandatory': { 'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true } }
-  );
-}
-
-
-function gotLocalDescription(description){
-  pc.setLocalDescription(description);
-  sendMessage(description);
-}
-
-function gotIceCandidate(event){
-  if (event.candidate) {
-    sendMessage({
-      type: 'candidate',
-      label: event.candidate.sdpMLineIndex,
-      id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate
-    });
+    state.pc = new PeerConnection(null);
+    state.pc.addStream(stream);
+    state.pc.onicecandidate = gotIceCandidate;
+    state.pc.onaddstream = gotRemoteStream;
   }
-}
 
-function gotRemoteStream(event){
-  document.getElementById("remoteVideo").src = URL.createObjectURL(event.stream);
-}
+  function gotIceCandidate(event){
+    if (event.candidate) {
+      state.sendMessage({
+        type: 'candidate',
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate
+      });
+    }
+  }
+
+  function gotRemoteStream(event){
+    remoteVideo.src = URL.createObjectURL(event.stream);
+    callButton.style.color = 'red';
+  }
+
+  callButton.addEventListener('click', manageCall);
+  
+  function manageCall() {
+    if (state.inCall) {
+      closeCall();
+      state.inCall = false;
+    } else {
+      createOffer();
+      state.inCall = true;
+    }  
+  }
+
+  // Step 2. createOffer
+  function createOffer() {
+    state.pc.createOffer(
+      gotLocalDescription, 
+      function(error) { console.log(error) }, 
+      { 'mandatory': { 'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true } }
+    );
+  }
+
+  // Step 3. createAnswer
+  state.createAnswer = function createAnswer() {
+    state.pc.createAnswer(
+      gotLocalDescription,
+      function(error) { console.log(error) }, 
+      { 'mandatory': { 'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true } }
+    );
+  }
+
+  function gotLocalDescription(description){
+    state.pc.setLocalDescription(description);
+    state.sendMessage(description);
+  }
+
+  function closeCall(){
+    remoteVideo.pause();
+    state.pc.close();
+    callButton.style.color = '';
+    console.log('call ended');
+  }
+
+}())
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,19 +1,24 @@
-var socket = io.connect('', {port: 3000});
+(function(){
+  var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+  var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+  var socket = io.connect('', {port: 3000});
 
-function sendMessage(message){
-  socket.emit('message', message);
-}
-
-socket.on('message', function (message){
-  if (message.type === 'offer') {
-    pc.setRemoteDescription(new SessionDescription(message));
-    createAnswer();
-  } 
-  else if (message.type === 'answer') {
-    pc.setRemoteDescription(new SessionDescription(message));
-  } 
-  else if (message.type === 'candidate') {
-    var candidate = new IceCandidate({sdpMLineIndex: message.label, candidate: message.candidate});
-    pc.addIceCandidate(candidate);
+  state.sendMessage = function sendMessage(message){
+    socket.emit('message', message);
   }
-});
+
+  socket.on('message', function (message){
+    if (message.type === 'offer') {
+      state.pc.setRemoteDescription(new SessionDescription(message));
+      state.createAnswer();
+    } 
+    else if (message.type === 'answer') {
+      state.pc.setRemoteDescription(new SessionDescription(message));
+    } 
+    else if (message.type === 'candidate') {
+      var candidate = new IceCandidate({sdpMLineIndex: message.label, candidate: message.candidate});
+      state.pc.addIceCandidate(candidate);
+    }
+  });
+
+}())
